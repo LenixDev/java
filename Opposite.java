afefec7749eecb54d1b30e604d1d5039dc7d6323
Lenix <lenix01x@gmail.com>
2025-11-14 00:30:09 +0100

    #

---
+++ Opposite.java
@@ -0,0 +1,9 @@
+public class Opposite {
+  public static void main(int a, int b) {
+    if (a + b == 0) {
+      System.out.println(a + " et " + b + " sont opposés");
+    } else {
+      System.out.println(a + " et " + b + " ne sont pas opposés");
+    }
+  }
+}
